86c80b60ab96230e64999fdf6c0462310d0d80e0
carlosd <carlosd@clubegazetadopovo.com.br>
2024-03-22 10:09:37 -0300

    finished customizing the Signin component and created the Signup component

--- src/screens/Signup.tsx
+++ src/screens/Signup.tsx
@@ -4,7 +4,7 @@ import LogoSvg from '@assets/logo.svg'
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
-export function SignIn() {
+export function SignUp() {
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
       <VStack flex={1} bg='gray.700' paddingX={10} pb={16}>
@@ -29,8 +29,11 @@ export function SignIn() {
             mb={6}
             fontFamily='heading'
           >
-            Acesse sua conta
+            Crie sua conta
           </Heading>
+          <Input
+            placeholder="Nome"
+          />
           <Input
             placeholder="E-mail"
             keyboardType="email-address"
@@ -40,18 +43,13 @@ export function SignIn() {
             placeholder="Senha"
             secureTextEntry
           />
-          <Button title='Acessar' />
+          <Button title='Criar e acessar' />
         </Center>
-
-        <Center mt={24}>
-          <Text color='gray.100' fontSize='sm' mb={3} fontFamily='body'>
-            Ainda não tem acesso?
-          </Text>
-          <Button
-            title='Criar conta'
-            variant='outline'
-          />
-        </Center>
+        <Button
+          title='Voltar para o login'
+          variant='outline'
+          mt={24}
+        />
       </VStack>
     </ScrollView>
   )

71ed90a8750ef38c724f40ec2a02c03dae0cacb6
carlosd <carlosd@clubegazetadopovo.com.br>
2024-03-23 11:09:50 -0300

    setup app routes and customized the tab bottom menu

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Text, View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider } from 'native-base';
 import { Loading } from '@components/Loading';
